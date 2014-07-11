02ac3d6465ad4f68cbe55060b7531b54faf4b0b7
ngotuantruong <ngotruong.fit@gmail.com>
2014-07-11 13:49:10 +0700

    Update drbm.m

--- drbm.m
+++ drbm.m
@@ -1,34 +1,34 @@
 function params = drbm(params, data,opts)
 tic;
-        m = size(data.train_x, 1);
+        m  = size(data.train_x, 1);
         kk = randperm(m); 
         
         for l = 1 : m
 %            disp(['example= ',num2str(l)])            
-            x1 = data.train_x(kk(l), :);
-            y1=data.train_y(kk(l), :);   
-            tmp=params.c+params.W*x1';
-            emu= bsxfun(@plus, params.U, tmp);
+            x1       = data.train_x(kk(l), :);
+            y1       = data.train_y(kk(l), :);   
+            tmp      = params.c+params.W*x1';
+            emu      = bsxfun(@plus, params.U, tmp);
             % 1. Compute p(h|y,x) matrix [numclass x Dh]
-            ph=1./(1+exp(-emu));
+            ph       = 1./(1+exp(-emu));
             % 2. Compute p(h|y=k,x) vector row [1 x Dh]       
-            pos=ph(:,find(y1==1,1));
+            pos      = ph(:,find(y1==1,1));
             % 3. Compute p(y|x) vector column [numclass x 1]
-            vt1=find(emu<10);          
-            emu(vt1)=log(1+exp(emu(vt1)));        
+            vt1      = find(emu<10);          
+            emu(vt1) =log(1+exp(emu(vt1)));        
                 
-            mu=sum(emu)+params.d';           
-            maximum=max(mu);
+            mu       = sum(emu)+params.d';           
+            maximum  = max(mu);
             if maximum<-745 || maximum>707.4 || min(mu)<-745
                  mu=mu+(707.4-maximum);
             end
-            e=exp(mu);
-            pyx=e/sum(e);
+            e        = exp(mu);
+            pyx      = e/sum(e);
 
             % 4. sigma( p(y'|x).p(hj'=1|x,y') )  vector row [1 x Dh]
          
-            s1 = bsxfun(@times, ph, pyx);
-            s=sum(s1,2);
+            s1       = bsxfun(@times, ph, pyx);
+            s        = sum(s1,2);
                            
             % Update parameters
       
