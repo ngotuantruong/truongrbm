b3b39ed6a355cc5a69bb7a9175ec4369f230a933
ngotuantruong <ngotruong.fit@gmail.com>
2014-07-11 13:47:01 +0700

    Update convert.m

--- convert.m
+++ convert.m
@@ -1,8 +1,8 @@
 function labels = convert( inputdata )
-    [r,c]=size(inputdata);
-    labels=zeros(r,1);
+    [r,c] = size(inputdata);
+    labels= zeros(r,1);
     for i=1:r
-        labels(i)=find(inputdata(i,:),1,'first');
+        labels(i) = find(inputdata(i,:),1,'first');
     end
 end
 
